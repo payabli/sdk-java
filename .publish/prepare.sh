b2fc13bb6bccd21a71a286e955976be8bddc42bb
payablijah <elijah@payabli.com>
2025-08-29 11:06:25 -0400

    updated key var refs

--- .publish/prepare.sh
+++ .publish/prepare.sh
@@ -1,8 +1,8 @@
 # Write key ring file
-echo "$MAVEN_SIGNATURE_SECRET_KEY" > armored_key.asc
+echo "$SDK_MAVEN_GPG_SECRET_KEY" > armored_key.asc
 gpg -o publish_key.gpg --dearmor armored_key.asc
 
 # Generate gradle.properties file
-echo "signing.keyId=$MAVEN_SIGNATURE_KID" > gradle.properties
+echo "signing.keyId=$SDK_MAVEN_GPG_SECRET_KEY_ID" > gradle.properties
 echo "signing.secretKeyRingFile=publish_key.gpg" >> gradle.properties
-echo "signing.password=$MAVEN_SIGNATURE_PASSWORD" >> gradle.properties
+echo "signing.password=$SDK_MAVEN_GPG_PASSWORD" >> gradle.properties
