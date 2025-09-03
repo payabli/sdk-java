06b8998b9c5987303497f52d24007cc998273fcc
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-09-03 16:27:04 +0000

    Release 0.0.285

--- src/main/java/com/payabli/api/core/ClientOptions.java
+++ src/main/java/com/payabli/api/core/ClientOptions.java
@@ -32,10 +32,10 @@ public final class ClientOptions {
         this.headers.putAll(headers);
         this.headers.putAll(new HashMap<String, String>() {
             {
-                put("User-Agent", "com.payabli:sdk-java/0.0.284");
+                put("User-Agent", "com.payabli:sdk-java/0.0.285");
                 put("X-Fern-Language", "JAVA");
                 put("X-Fern-SDK-Name", "com.payabli.fern:api-sdk");
-                put("X-Fern-SDK-Version", "0.0.284");
+                put("X-Fern-SDK-Version", "0.0.285");
             }
         });
         this.headerSuppliers = headerSuppliers;
